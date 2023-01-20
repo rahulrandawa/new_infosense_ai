ea969eaa73480b64c2ca46f21134cf706a321e99
apeksha4692 <apkesha.infograins@gmail.com>
2023-01-20 14:15:00 +0530

    updateAdmin20Jan

--- assets/js/main.js
+++ assets/js/main.js
@@ -130,53 +130,53 @@
     });
   }
 
-  if (select('.quill-editor-full')) {
-    new Quill(".quill-editor-full", {
-      modules: {
-        toolbar: [
-          [{
-            font: []
-          }, {
-            size: []
-          }],
-          ["bold", "italic", "underline", "strike"],
-          [{
-              color: []
-            },
-            {
-              background: []
-            }
-          ],
-          [{
-              script: "super"
-            },
-            {
-              script: "sub"
-            }
-          ],
-          [{
-              list: "ordered"
-            },
-            {
-              list: "bullet"
-            },
-            {
-              indent: "-1"
-            },
-            {
-              indent: "+1"
-            }
-          ],
-          ["direction", {
-            align: []
-          }],
-          ["link", "image", "video"],
-          ["clean"]
-        ]
-      },
-      theme: "snow"
-    });
-  }
+  // if (select('.quill-editor-full')) {
+  //   new Quill(".quill-editor-full", {
+  //     modules: {
+  //       toolbar: [
+  //         [{
+  //           font: []
+  //         }, {
+  //           size: []
+  //         }],
+  //         ["bold", "italic", "underline", "strike"],
+  //         [{
+  //             color: []
+  //           },
+  //           {
+  //             background: []
+  //           }
+  //         ],
+  //         [{
+  //             script: "super"
+  //           },
+  //           {
+  //             script: "sub"
+  //           }
+  //         ],
+  //         [{
+  //             list: "ordered"
+  //           },
+  //           {
+  //             list: "bullet"
+  //           },
+  //           {
+  //             indent: "-1"
+  //           },
+  //           {
+  //             indent: "+1"
+  //           }
+  //         ],
+  //         ["direction", {
+  //           align: []
+  //         }],
+  //         ["link", "image", "video"],
+  //         ["clean"]
+  //       ]
+  //     },
+  //     theme: "snow"
+  //   });
+  // }
 
   /**
    * Initiate TinyMCE Editor
